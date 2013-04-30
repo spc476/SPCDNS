62357a86b1f9211eea261cf319c0838105ff65fe
Sean Conner <spc@conman.org>
2013-04-29 23:26:48 -0400

    Update version number for the Lua module

--- src/luadns.c
+++ src/luadns.c
@@ -758,8 +758,8 @@ int luaopen_org_conman_dns(lua_State *L)
   lua_pushliteral(L,"Encode/Decode and send queries via DNS");
   lua_setfield(L,-2,"DESCRIPTION");
   
-  lua_pushliteral(L,"0.0.1");
-  lua_setfield(L,-2,"VERSION");
+  lua_pushliteral(L,"1.0.5");
+  lua_setfield(L,-2,"_VERSION");
   
   return 1;
 }
