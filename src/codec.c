cf3f8442c2abccf12386b862b693143d6e1761ae
Alper Akcan <alper.akcan@gmail.com>
2023-11-11 21:27:15 +0300

    dns_encode to support rr_private

--- src/codec.c
+++ src/codec.c
@@ -957,7 +957,13 @@ static dns_rcode_t encode_answer(
     
     case RR_NULL: rc = encode_rr_x(data,&answer->x); break;
     
-    default: rc = RCODE_NOT_IMPLEMENTED; break;
+    default:
+      if ((answer->generic.type >= RR_PRIVATE) && (answer->generic.type < RR_UNKNOWN))
+      {
+        rc = encode_rr_x(data,&answer->x);
+        break;
+      }
+      rc = RCODE_NOT_IMPLEMENTED; break;
   }
   
   if (rc != RCODE_OKAY)
